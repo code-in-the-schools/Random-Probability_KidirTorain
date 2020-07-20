06e59b23b6f6154f4d2efa096d6e77946b6b62b4
Kidir Torain <kktorainnn@gmail.com>
2020-07-20 18:32:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+# create list 10
+# create list 100
+# create list 100,000
+
+num1 = random.randint(10)(start)
+n = (range) # random int between 1 and 6
+for n in range 
+  print(n) #print numbers in 1 through 6 
+# percentage number in loop 
+
+{:.2%}.format(rate)
+
+random.ranidint(100)
+
+
+
+
+
