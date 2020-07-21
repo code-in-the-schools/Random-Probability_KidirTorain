dd3e7146cddc38ecc9bbf330ef5ff2201b2b51ef
Kidir Torain <kktorainnn@gmail.com>
2020-07-21 15:41:40 +0000

    coding

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ import random
 # create list 100
 # create list 100,000
 
-num1 = random.randint(10)(start)
+num1 = random.randint(10)
 n = (range) # random int between 1 and 6
-for n in range 
+for n in range(10):
   print(n) #print numbers in 1 through 6 
 # percentage number in loop 
 
-{:.2%}.format(rate)
 
-random.ranidint(100)
+#{:.2%}.format(rate)
+
+num2 = random.ranidint(100)
+for n in range(100):
+  print(n)
 
 
+  num3 = random.randint(100000)
+for n in range(100000):
+  print(n)
 
 
 
